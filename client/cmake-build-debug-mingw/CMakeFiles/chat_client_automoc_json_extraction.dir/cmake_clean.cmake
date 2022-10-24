6b8989cf9f61ea637cf5c0f1882022593845b98f
Rocco <grigoreandrey23@gmail.com>
2022-10-24 15:46:21 +0300

    basic qt client working and compiling

---
+++ client/cmake-build-debug-mingw/CMakeFiles/chat_client_automoc_json_extraction.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chat_client_automoc_json_extraction"
+  "meta_types/chat_client_json_file_list.txt"
+  "meta_types/chat_client_json_file_list.txt.timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/chat_client_automoc_json_extraction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ client/cmake-build-debug-mingw/CMakeFiles/chat_client_qmltyperegistration.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chat_client_qmltyperegistration"
+  "chat_client/chat_client.qmltypes"
+  "chat_client_qmltyperegistrations.cpp"
+  "meta_types/qt6chat_client_debug_metatypes.json"
+  "meta_types/qt6chat_client_debug_metatypes.json.gen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/chat_client_qmltyperegistration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
