f0b4ce98e22685ee45848a3bd74883ca0c0c3c7c
Andrei Grigore <31166094+Rocco2300@users.noreply.github.com>
2022-10-24 16:48:46 +0300

    Delete client/cmake-build-debug-mingw directory

--- client/cmake-build-debug-mingw/CMakeFiles/chat_client_qmlimportscan.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  ".qt_plugins/Qt6_QmlPlugins_Imports_chat_client.cmake"
-  "CMakeFiles/chat_client_qmlimportscan"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/chat_client_qmlimportscan.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- client/cmake-build-debug-mingw/CMakeFiles/chat_client_qmllint_json.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/chat_client_qmllint_json"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/chat_client_qmllint_json.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
